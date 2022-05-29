feff421168e2c1f9bb472379f56deb9b1dde98f1
amcchua <amcchua@mymail.mapua.edu.ph>
2022-05-29 17:30:09 +0800

    updated the controller

--- app/src/main/java/com/example/prcmobileapp/WebViewController.java
+++ app/src/main/java/com/example/prcmobileapp/WebViewController.java
@@ -6,8 +6,6 @@ import android.webkit.WebViewClient;
 public class WebViewController extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        // loadurl function will load the
-        // url we will provide to our webview
         view.loadUrl(url);
         return true;
     }
