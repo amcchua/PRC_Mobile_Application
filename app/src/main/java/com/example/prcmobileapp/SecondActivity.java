d1c16c93c2ad027fca96963bd4789f181b77f7d4
Torio <jpstorio@mymail.mapua.edu.ph>
2022-07-08 14:39:10 +0800

    Search Functionality Fixed Approve Button Fixed Profile Photo Upload Fixed Certificate Viewing: Link working but PDF is not visible ^ Created a dialog for notice

--- app/src/main/java/com/example/prcmobileapp/SecondActivity.java
+++ app/src/main/java/com/example/prcmobileapp/SecondActivity.java
@@ -1,19 +1,167 @@
 package com.example.prcmobileapp;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    final Context myApp = this;
+    WebView webView;
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        WebView webView = (WebView) findViewById(R.id.web_view_main);
+        webView = (WebView) findViewById(R.id.web_view_main);
         webView.loadUrl("https://www.digitalmonitoringsystem.online/");  // Url of portal is passed
         webView.requestFocus();
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+
+
+
         webView.setWebViewClient(new WebViewController());
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            //For Certificate
+            public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, android.os.Message resultMsg)
+            {
+                AlertDialog.Builder alert = new AlertDialog.Builder(myApp);
+                alert.setTitle("Certificate Notice");
+                alert.setMessage("Certificates can only be viewed on the browser.");
+                alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+                //D lumalabas yung pdf pero naoopen link
+                //WebView.HitTestResult result = view.getHitTestResult();
+                //String data = result.getExtra();
+                //Context context = webView.getContext();
+                //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                //myApp.startActivity(browserIntent);
+                return false;
+            }
+
+            //For Profile Photo Upload
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For JSAlerts
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+
+                new AlertDialog.Builder(myApp)
+                        .setTitle("Mobile APP PRC")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener()
+                                {
+
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        result.confirm();
+                                    }
+                                })
+                        .setNegativeButton(android.R.string.cancel,
+                                new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        result.cancel();
+                                    }
+                                })
+                        .create()
+                        .show();
+
+                return true;
+            }
+        });
+
     }
+
+    //For Mobile Back Button
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    //For Upload Photo
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(getApplicationContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/com/example/prcmobileapp/WebViewController.java
+++ app/src/main/java/com/example/prcmobileapp/WebViewController.java
@@ -1,12 +1,15 @@
 package com.example.prcmobileapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebViewController extends WebViewClient {
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        view.loadUrl(url);
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
         return true;
-    }
+     }
 }
